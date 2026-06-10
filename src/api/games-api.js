5090d632c7e13909459494dcd5c3f93358472078
Juan <juan.isidro.menendez@gmail.com>
2026-06-10 14:24:39 +0200

    api url call

--- src/api/games-api.js
+++ src/api/games-api.js
@@ -1,1 +1,6 @@
-// Servicio API: llamadas a MMOBomb (https://www.mmobomb.com/api1/games)
+const apiURL = 'https://www.mmobomb.com/api1/games'
+
+fetch(apiURL)
+        .then(response => response.json()) 
+        .then(data => console.log(data))
+        .catch(error => console.log('Error, error'))
