b46662619bbcf8d15c0d78e43228f14b5f2ec93b
iulian640 <iuliantim21@gmail.com>
2026-06-11 09:50:42 +0200

    feat: firebase auth requestç

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,8 +1,16 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from 'firebase.js'
+import { auth } from '../api/firebase.js'
 export const useAuthStore = defineStore('auth', () => {
     const user = ref(null)
     const loading = ref(true)
+
+    onAuthStateChanged(auth, (firebaseUser) => {
+
+        user.value = firebaseUser
+        loading.value = false
+    })
+    return { user, loading )  
 })
+
