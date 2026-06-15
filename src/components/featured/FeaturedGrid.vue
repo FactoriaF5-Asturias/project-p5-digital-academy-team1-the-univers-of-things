9b7042150c9ce8dc2039863220e9c8f41b5961cc
iulian640 <iuliantim21@gmail.com>
2026-06-15 11:49:47 +0200

    feat: added game cards for featured view.

---
+++ src/components/featured/FeaturedGrid.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import ItemCard from '@/components/items/ItemCard.vue'
+
+    const games = [
+  { id: 1, title: 'Juego 1', short_description: 'Descripción', thumbnail: '', genre: 'Acción', platform: 'PC' },
+  { id: 2, title: 'Juego 2', short_description: 'Descripción', thumbnail: '', genre: 'RPG', platform: 'PS5' },
+  { id: 3, title: 'Juego 3', short_description: 'Descripción', thumbnail: '', genre: 'FPS', platform: 'Xbox' },
+]
+</script>
+
+<template>
+    <div class="featured-grid">
+        <ItemCard
+        v-for="game in games"
+        :key="game.id"
+        :game="game"
+        />
+    </div>
+</template>
--- src/views/FeaturedView.vue
+++ src/views/FeaturedView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import MainLayout from '@/layouts/MainLayout.vue'
 import FeaturedHero from '@/components/featured/FeaturedHero.vue'
+import FeaturedGrid from '@/components/featured/FeaturedGrid.vue'
 </script>
 <template>
     <MainLayout>
         <FeaturedHero></FeaturedHero>
+        <FeaturedGrid></FeaturedGrid>
     </MainLayout>
     </template>
 
