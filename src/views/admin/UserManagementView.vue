5f6053cff4a54a0c8bd9e5b91abfaa7e55499393
iulian640 <iuliantim21@gmail.com>
2026-06-17 13:49:17 +0200

    style: style search input and add list transition animation

--- src/views/admin/UserManagementView.vue
+++ src/views/admin/UserManagementView.vue
@@ -21,32 +21,27 @@ const filteredUsers = computed(() =>
   <AdminLayout>
     <section class="user-management">
       <h1 class="user-management__title">Gestión de Usuarios</h1>
-      <input v-model="searchQuery" />
+      <input v-model="searchQuery" placeholder="Introduce usuario o email" class="user-management__search" />
       <table class="user-management__table">
-        <thead>
-          <tr>
-            <th scope="col">Avatar</th>
-            <th scope="col">Usuario</th>
-            <th scope="col">Correo</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Accion</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="user in filteredUsers" :key="user.id">
-            <td>img</td>
-            <td>{{ user.username }}</td>
-            <td>{{ user.email }}</td>
-            <td>
-              <span
-                class="user-management__badge user-management__badge--active"
-                >PERMITIDO</span
-              >
-            </td>
-            <td><button class="user-management__btn">RESTRINGIR</button></td>
-          </tr>
-        </tbody>
-      </table>
+  <thead>
+    <tr>
+      <th scope="col">Avatar</th>
+      <th scope="col">Usuario</th>
+      <th scope="col">Correo</th>
+      <th scope="col">Estado</th>
+      <th scope="col">Accion</th>
+    </tr>
+  </thead>
+  <TransitionGroup name="list" tag="tbody">
+    <tr v-for="user in filteredUsers" :key="user.uid">
+      <td>img</td>
+      <td>{{ user.username }}</td>
+      <td>{{ user.email }}</td>
+      <td><span class="user-management__badge user-management__badge--active">PERMITIDO</span></td>
+      <td><button class="user-management__btn">RESTRINGIR</button></td>
+    </tr>
+  </TransitionGroup>
+</table>
       <p v-if="filteredUsers.length === 0">No se encontraron resultados</p>
     </section>
   </AdminLayout>
