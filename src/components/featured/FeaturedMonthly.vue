a04c7a968af7a7f0739115ce95e1368b59d78167
iulian640 <iuliantim21@gmail.com>
2026-06-18 15:25:06 +0200

    style: game of the month aligne and styled

--- src/components/featured/FeaturedMonthly.vue
+++ src/components/featured/FeaturedMonthly.vue
@@ -14,9 +14,30 @@ const gameMonth = computed(() => featuredStore.gameOfTheMonth)
 
 <template>
   <div class="featured-monthly" v-if="gameMonth">
+    <div class="featured-monthly__text">
     <span class="featured-monthly__tag">• DESTACADO DEL MES</span>
     <h1 class="featured-monthly__title">{{ gameMonth.title }}</h1>
     <p class="featured-monthly__descripcion">{{ gameMonth.short_description }}</p>
-    <img :src="gameMonth.thumbnail" />
+  </div>
+    <img :src="gameMonth.thumbnail" class="featured-monthly__image"/>
   </div>
 </template>
+
+<style scoped lang="scss">
+
+.featured-monthly {
+  display: flex;
+  max-width: 900px;
+  margin: 0 auto;
+  flex-direction: row-reverse;
+  gap: 2rem;
+  align-items: center;
+
+  &__text {
+  }
+
+  &__image {
+    width: 300px;
+  }
+}
+</style>
